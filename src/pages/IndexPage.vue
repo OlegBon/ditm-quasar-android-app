89bbe7d918c5799294740526d2ec99192c39b130
OlegBon <oleg_bon@ukr.net>
2025-02-07 15:54:25 +0200

    update Home

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,23 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="column column-container">
+    <h1>Hello!</h1>
     <img
+      class="home-img"
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      src="~assets/home-banner.jpg"
+      style="width: 100%; height: 100%"
     />
+    <p class="p-16">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit odit non aut, praesentium
+      explicabo eligendi quaerat assumenda dolor tempora quae omnis quasi molestias? Nobis enim
+      omnis laudantium. Distinctio, in ea, officia laborum possimus facere dolor fugiat quas nostrum
+      nobis rem? Provident laboriosam porro veniam aspernatur fugit. Blanditiis, illo aperiam ad
+      earum commodi voluptatum cum consequuntur mollitia tempore dolor. Incidunt vel doloribus ad
+      nesciunt sed ipsum quas, sapiente, maxime assumenda illum inventore asperiores quisquam
+      perferendis quis dolorem quaerat impedit velit facere deserunt expedita. Omnis aspernatur
+      quas, maiores est nesciunt illo, minus cupiditate ab ipsum sapiente saepe. Ad voluptatibus
+      rerum iste porro.
+    </p>
   </q-page>
 </template>
 
