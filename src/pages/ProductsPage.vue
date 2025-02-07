25c17aba6138b10ced0a4e93001b4c4d2cbafe3d
OlegBon <oleg_bon@ukr.net>
2025-02-07 16:19:41 +0200

    add search for Products page

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,10 +1,13 @@
 <template>
   <q-page>
+    <q-input v-model="search" debounce="500" filled placeholder="Search" class="search-products">
+      <template v-slot:append>
+        <q-icon name="search" />
+      </template>
+    </q-input>
+
     <div>
-      <h1>All Products</h1>
-    </div>
-    <div>
-      <q-list v-for="product in products" :key="product.id" bordered separator>
+      <q-list v-for="product in filteredProducts" :key="product.id" bordered separator>
         <q-item clickable v-ripple>
           <q-item-section>{{ product.title }}</q-item-section>
         </q-item>
@@ -14,9 +17,20 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { api } from 'boot/axios'
+
+const search = ref('') // Додаємо змінну search
 let products = ref([])
+
+// Фільтровані продукти на основі пошуку
+const filteredProducts = computed(() => {
+  if (!search.value) return products.value
+  return products.value.filter((product) =>
+    product.title.toLowerCase().includes(search.value.toLowerCase()),
+  )
+})
+
 api('https://dummyjson.com/products').then(({ data }) => {
   products.value = data.products
 })
