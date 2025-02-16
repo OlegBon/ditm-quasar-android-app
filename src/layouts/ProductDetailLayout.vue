c172bbf7642afa0ff4580b5bb4764e938925d828
OlegBon <oleg_bon@ukr.net>
2025-02-16 18:53:09 +0200

    go on Prodact card in Cart and add quantity of products in the cart (basket) for Prodact card

--- src/layouts/ProductDetailLayout.vue
+++ src/layouts/ProductDetailLayout.vue
@@ -10,7 +10,12 @@
 
       <q-tabs>
         <q-route-tab icon="ion-basket" to="/products" replace label="Products" />
-        <q-route-tab v-if="isLoggedIn" icon="ion-cart" to="/cart" replace label="Cart" />
+        <q-route-tab v-if="isLoggedIn">
+          <div class="cart-icon-wrapper">
+            <q-route-tab icon="ion-cart" to="/cart" replace label="Cart" />
+            <q-badge color="red" floating>{{ uniqueCartItemsCount }}</q-badge>
+          </div>
+        </q-route-tab>
         <q-route-tab v-else icon="login" to="/login" replace label="Login" />
       </q-tabs>
     </q-header>
@@ -38,6 +43,7 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { useUserStore } from '../store/userStore'
+import { useCartStore } from '../store/cartStore'
 import Sidebar from './SidebarLayout.vue'
 import { getCurrentDate } from '../utils/date'
 import BackToTopButton from '../components/BackToTopButton.vue'
@@ -51,8 +57,10 @@ const { title } = defineProps({
 
 const leftDrawerOpen = ref(false)
 const userStore = useUserStore()
+const cartStore = useCartStore()
 
 const isLoggedIn = computed(() => !!userStore.user)
+const uniqueCartItemsCount = computed(() => cartStore.items.length)
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -3,11 +3,15 @@
     <h1>Cart</h1>
     <q-list>
       <q-item v-for="item in cartItems" :key="item.id" bordered>
-        <q-item-section>
+        <q-item-section clickable @click="goToProduct(item.id)">
           <img :src="item.thumbnail" alt="Product image" class="cart-img" />
         </q-item-section>
-        <q-item-section>{{ item.title }}</q-item-section>
-        <q-item-section>{{ item.price }}</q-item-section>
+        <q-item-section clickable @click="goToProduct(item.id)">
+          {{ item.title }}
+        </q-item-section>
+        <q-item-section clickable @click="goToProduct(item.id)">
+          {{ item.price }}
+        </q-item-section>
         <q-item-section>
           <q-input
             v-model.number="item.quantity"
@@ -17,7 +21,7 @@
           />
         </q-item-section>
         <q-item-section>
-          <q-btn color="negative" @click="removeFromCart(item.id)">
+          <q-btn color="negative" @click.stop="removeFromCart(item.id)">
             <q-icon name="delete" />
           </q-btn>
         </q-item-section>
@@ -30,9 +34,11 @@
 
 <script setup>
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { useCartStore } from '../store/cartStore'
 
 const cartStore = useCartStore()
+const router = useRouter()
 
 const cartItems = computed(() => cartStore.items)
 const totalPrice = computed(() => cartStore.totalPrice)
@@ -47,5 +53,9 @@ function clearCart() {
 
 function updateQuantity(productId, quantity) {
   cartStore.updateQuantity(productId, quantity)
+}
+
+function goToProduct(productId) {
+  router.push(`/products/${productId}`)
 }
 </script>
